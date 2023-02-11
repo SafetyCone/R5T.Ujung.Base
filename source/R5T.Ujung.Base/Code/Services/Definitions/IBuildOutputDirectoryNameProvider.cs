2357f6135713276cdb12d35d3bcad3122e1e40e2
davidcoats <david@getsafetycone.com>
2023-02-10 17:51:54 -0800

    Interim changes.

--- source/R5T.Ujung.Base/Code/Services/Definitions/IBuildOutputDirectoryNameProvider.cs
+++ source/R5T.Ujung.Base/Code/Services/Definitions/IBuildOutputDirectoryNameProvider.cs
@@ -2,10 +2,11 @@ using System;using R5T.T0064;
 
 
 namespace R5T.Ujung
-{[ServiceDefinitionMarker]
+{
     /// <summary>
     /// Generally the "bin" directory.
     /// </summary>
+    [ServiceDefinitionMarker]
     public interface IBuildOutputDirectoryNameProvider:IServiceDefinition
     {
         string GetBuildOutputDirectoryName();
